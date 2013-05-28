97320ec646e5aa6eac8f5fce321f704a3ae56915
psahni <prashant.sahni5@gmail.com>
2013-05-28 19:22:59 +0530

    run a command on the remote server

---
+++ remote_server_command_net_ssh.rb
@@ -0,0 +1,22 @@
+##############################################################
+# Run a remote command on the remote server
+##############################################################
+
+require 'rubygems'
+require 'net/ssh'
+
+@hostname = "staging.inxero.com"
+@username = "inxeroadmin"
+@password = "3PUth4Du"
+@cmd = "ls -al"
+
+
+
+begin
+ssh = Net::SSH.start(@hostname, @username, :password =>      @password)
+res = ssh.exec!(@cmd)
+ssh.close
+puts res
+rescue
+  puts "Unable to connect to #{@hostname} using #{@username}/#  {@password}"
+end
