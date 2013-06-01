d7eeec85354ac06c137cbd8561f1dcbf2974ad1d
psahni <prashant.sahni5@gmail.com>
2013-06-01 10:34:23 +0530

    date time diff example added

--- active_rceord_with_ruby_example.rb
+++ active_rceord_with_ruby_example.rb
@@ -28,8 +28,20 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password,                  :if => :password_required?
   
   
+
+	def self.create_user
+		user = User.new(:email => 'prashantsahni5@gmail.com', :username => 'psahni', :password => 'pass123', :password_confirmation => 'pass123')		
+  	if user.valid?
+	  	 user.save
+	  else
+		 puts "user is not valid"
+		 p user.errors.full_messages
+	  end
+
+	end
+	
   
-  
+
   private
   
   def password_required?
@@ -39,15 +51,20 @@ class User < ActiveRecord::Base
   
 end
 
-user = User.new(:email => 'prashantsahni5@gmail.com', :username => 'psahni', :password => 'pass123', :password_confirmation => 'pass123')
+
+#User.create_user
+p User.all
+
+
+https://github.com/gunark/rubycas-client/blob/master/examples/rails/app/controllers/simple_example_controller.rb
+https://github.com/rubycas/rubycas-client
+https://github.com/rubycas/rubycas-client-rails
+https://github.com/rubycas/rubycas-server/wiki/Installation
+https://github.com/rubycas/rubycas-server/wiki/Usage-Scenarios
+https://github.com/rubycas/rubycas-client-rails/blob/master/lib/rubycas-client-rails.rb
+http://rubyglasses.blogspot.in/2009/12/login-to-rubycas-from-form.html
+
 
 #logger = Logger.new(STDOUT)
 #logger.warn('This message goes to stdout')
-
-if user.valid?
-	 user.save
-else
-	puts "user is not valid"
-	p user.errors.full_messages
-end
 
---
+++ datetime_diff_example.rb
@@ -0,0 +1,45 @@
+ require 'date'
+
+
+
+
+# Works With Rails
+#~ def distance_between(start_date, end_date)
+    #~ difference = end_date.to_i - start_date.to_i  # Total Secs
+    #~ seconds    =  difference % 60                 # Remaining Secs
+    #~ difference = (difference - seconds) / 60      # Total Mins
+    #~ minutes    =  difference % 60                 # Remaining Mins
+    #~ difference = (difference - minutes) / 60      # Total Hours
+    #~ hours      =  difference % 24                 # Remaining Hours
+    #~ difference = (difference - hours)   / 24      # Total Days = Total Hours/24
+    #~ days       =  difference % 7                  # Remaining Days
+    #~ weeks      = (difference - days)    /  7      # Total Weeks
+    
+    #~ return "#{hours} hours, #{minutes} minutes, #{seconds} seconds"
+  #~ end
+  
+#  distance_between(d1, d2)
+ 
+ 
+ ## Simple Date Diff Ruby
+ 
+  d1 = Date.new(2012, 05, 12)  # Also works with DateTime, d1=DateTime.now;d1.day;d1.month;d1.year
+  d2 = Date.today
+ 
+  diff = (d2-d1)   # Rational Object
+ 
+  days = diff.to_i # Gives the no of days
+
+ 
+  year = days%12 
+ 
+  days_left = days%365
+  
+  months = days_left/30
+  
+  days_left = days_left%30
+  
+  
+ 
+  puts "#{ year } year #{ months } months #{ days_left } days"
+
