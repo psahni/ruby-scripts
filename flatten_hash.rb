f831d8cc5140099cbfeb48f1314bfdccdbcae6c6
Prashant Sahni <prashant.sahni5@gmail.com>
2015-02-03 16:02:15 +0530

    flatten hash

---
+++ flatten_hash.rb
@@ -0,0 +1,95 @@
+require 'active_support/core_ext/string'
+
+class Hash
+
+#  def flatten!
+#    _self = self   
+#    _self.each_pair do |key, value|
+#      if Array === value
+#        _self[key] =  Array.new
+#        singular = String(key).singularize
+#        value.each do |hash|          
+#          _self[key] << (hash[singular.to_sym] || hash[singular])           
+#        end
+#      end
+#    end
+#  end
+
+  def flattenize!
+    self.each_pair do |key, value|
+      p value
+      if flatable?(value)
+        self[key] = value.map(&:values).flatten
+      elsif Array === value
+        value.flattenize!
+      end
+    end
+  end
+  
+  def flatable?(array_hash)
+    Array === array_hash && array_hash.map(&:values).flatten.all?{|v| Hash === v}
+  end
+  
+end
+
+class Array
+  def flattenize!
+    self.each do |element|
+      if Hash === element
+        element.flattenize!
+      else
+        
+      end
+    end
+  end
+end
+
+hash = { 
+  attribute_one: "foo",
+  posts: [
+    { 'post' => { 'name' => "p1", content: "post one" } },
+    { post: { name: "p2", content: "post two" } },
+    { post: { name: "p3", content: "post three" } }
+  ]
+}
+
+#hash.flattenize!
+
+array1 = ['posts',
+     {
+         attribute_one: 'foo',
+         posts: [{title: 'post1'}, {title: 'post2'}],
+         countries: [
+             {country: {name: 'India', content: 'car one'}},
+             {country: {name: 'Austrailia', content: 'car two'}},
+             {country: {name: 'New Zealand', content: 'car three'}},
+             {country: {name: 'Mauritious', content: 'car three'}},
+         ]
+     }]
+
+#array1.flattenize!
+
+hash2 =   {
+        attribute_one: 'foo',
+        posts: [
+            {post: {name: 'p1', content: 'post one'}},
+            {post: {name: 'p2', content: 'post two'}},
+            {post: {name: 'p3', content: 'post three'}}
+        ],
+        cars: [
+            {car: {name: 'p1', content: 'car one'}},
+            {car: {name: 'p2', content: 'car two'}},
+            {car: {name: 'p3', content: 'car three'}}
+        ],
+        attribute_three: [1, 2,
+                          {
+                              attribute_one: 'foo',
+                              cars: [
+                                  {car: {name: 'p1', content: 'car one'}},
+                                  {car: {name: 'p2', content: 'car two'}},
+                                  {car: {name: 'p3', content: 'car three'}}
+                              ]
+                          }]
+    }
+     
+p hash2.flattenize!     
