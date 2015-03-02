aadfe235e15b3a36af6facae7e8a9e5e0d98779f
Prashant Sahni <prashant.sahni5@gmail.com>
2015-03-02 15:52:02 +0530

    error handling

--- headerLabs/lib/subword.rb
+++ headerLabs/lib/subword.rb
@@ -1,6 +1,6 @@
 require 'colorize'
 
-class WordInputReader
+class WordReader
 
   def initialize(lines)
     puts "Invalid Input" unless Array === lines
@@ -25,17 +25,21 @@ class Subword
     @word, @no = line.strip.split(/\s+/)
   end
 
-  def get_permutations
-    puts error_prefix + "Number greater than length of word" if @word.length < @no.to_i
-    puts error_prefix + "Illegal character in number" if @no.to_i == 0
+  def get_permutations   
+    return [error_prefix + "Word longer than 20 characters"] if @word.length > 20
+    return [error_prefix + "Number greater than length of word"]  if @word.length < @no.to_i
+    return [error_prefix + "Illegal character in number"]    if @no.to_i == 0 || @no.match(/[a-zA-Z]/)
+    return [error_prefix + "Illegal character in word"]      if @word.match(/\d+/)
     @word.chars.permutation(@no.to_i).to_a
   end
   
   def print_subwords    
-    get_permutations.each do |sword|
-      puts sword.join()
+    get_permutations.each do |sword|     
+      puts(Array === sword ? sword.join() : sword)
     end    
   end  
+  
+##--- Private ---
   
   private
   
@@ -53,5 +57,5 @@ def prompt(*args)
 end
 
 lines = prompt "Input line Press Enter and then Ctrl-D to quit:\n"
-WordInputReader.new(lines).print_subwords
+WordReader.new(lines).print_subwords
 
